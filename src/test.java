799ae2377701331eec1a7869a21825f73a6c7ab8
jetlee79 <piksaoul79@hotmail.com>
2015-06-02 21:38:58 +0200

    fichier test

---
+++ src/test.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Guillaume
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}

e77ebbf8ea8c89059b51850a313e340e3f9bf715
jetlee79 <piksaoul79@hotmail.com>
2015-06-03 13:57:45 +0200

    Pour montrer à Matt

--- src/test.java
+++ src/test.java
@@ -4,17 +4,17 @@
 
 /**
  * @author Guillaume
- *
+ * 
  */
 public class test {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Thanks Juju");
-		System.out.println("Fin");
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Thanks Juju");
+        System.out.println("Fin test");
+    }
 
 }

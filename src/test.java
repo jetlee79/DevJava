dba44318c631775ffdae6be9283e9aa8f26f7e76
jetlee79 <piksaoul79@hotmail.com>
2015-06-04 16:41:40 +0200

    COmmit depuis luna

--- src/test.java
+++ src/test.java
@@ -1,10 +1,10 @@
 /**
- * 
+ *
  */
 
 /**
  * @author Guillaume
- * 
+ *
  */
 public class test {
 
@@ -14,7 +14,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Thanks Juju");
-        System.out.println("Fin test");
+        System.out.println("Fin");
     }
 
 }
